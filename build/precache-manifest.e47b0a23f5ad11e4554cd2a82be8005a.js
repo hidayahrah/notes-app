b10e33f0275db768a9edc2cefdc5abe74fc23ff6
hidayahrah <hidayahrah97@gmail.com>
2023-02-10 17:09:56 +0800

    Create build to host

---
+++ build/precache-manifest.e47b0a23f5ad11e4554cd2a82be8005a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12af2f8ddc5a6b378a8eb39b5161479c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "275e230102dba3f4f08c",
+    "url": "/static/js/2.605b2e91.chunk.js"
+  },
+  {
+    "revision": "169f174b1563321fa1b2",
+    "url": "/static/js/main.4e9acd96.chunk.js"
+  },
+  {
+    "revision": "53aaeafb8f74ed958289",
+    "url": "/static/js/runtime-main.6166fd73.js"
+  }
+]);
